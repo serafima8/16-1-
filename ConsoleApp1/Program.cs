2cc27b10eaa8c91a1c5ecd1e4544f848538085cb
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-01-28 17:28:20 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            //DateTime date=DateTime.Now;
+            //Console.WriteLine(date.ToShortDateString());
+            //Console.WriteLine(date.ToString("dd MMM yyy"));
+            //Console.WriteLine(date.ToString("dd MMMM yyy"));
+            //Console.WriteLine(date.ToString("ddd dd MMM yyy"));
+            //Console.WriteLine(date.ToLongDateString());
+            //Console.WriteLine(date.ToLongTimeString());
+            //Console.WriteLine(date.ToShortTimeString());
+            //Console.WriteLine($"{date.ToShortDateString()} {date.ToLongTimeString()}");
+            //Console.WriteLine($"{date.ToString("dd MMMM yyy")} {date.ToShortTimeString()}");
+            //Console.WriteLine($"{date.ToString("dd MMMM yyy")} {date.ToLongTimeString()}");
+            //Console.WriteLine(date.ToString("R"));
+            //Console.WriteLine(date.ToString("s"));
+
+
+            //2
+
+            //Console.Write("Введите свой возраст: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //DateTime date = DateTime.Now;
+            //int year = DateTime.Now.Year - DateTime.Now.Month - age;
+            //Console.WriteLine($"Ты родился в {year} году");
+
+
+            //3
+
+            //Console.Write("Введите год, в котором вы родились: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //DateTime date = DateTime.Now;
+            //int age = DateTime.Now.Year - DateTime.Now.Month - year;
+            //Console.WriteLine($"Ваш возраст: {age}");
+
+            Console.ReadKey();
+        }
+    }
+}
